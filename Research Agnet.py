db38acfecc07ffc11afeabec1cc4daa98af4ba69
Zabby Heying <heyingz@fultonschools.org>
2025-12-01 02:01:44 +0000

    Final Edits

--- Research Agnet.py
+++ Research Agnet.py
@@ -4,14 +4,9 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_community.tools import DuckDuckGoSearchRun
 
 
-model = ChatOllama(
-    model="research-llm"
-)
-
-prompt = ChatPromptTemplate.from_template("""You are a helpful research AI. Use the following web search result: {context}. To write a concise, well-structured response to this question : {question}""")
-
+model = ChatOllama(model="research-llm")
+prompt = ChatPromptTemplate.from_template("""You are a helpful research AI. Use the following web search result: {context}. To write a concise, well-structured response to the question /"Who is : {question} /" """)
 parser = StrOutputParser()
-
 search = DuckDuckGoSearchRun()
 
 
@@ -19,12 +14,24 @@ chain = prompt | model | parser
 
 def generate_response(userText):
     context = search.invoke(f"Who is {userText}")
-
     response = chain.invoke({"question": f"Summarize information about {userText}", "context": context})
     return response
 
-userInput = input("Who would you like to learn about today? \n type exit to leave\n\n")
+def write_to_file(information, i):
+    file_name = "output.txt"
+    try: 
+        with open(file_name, 'a') as file:
+            file.write(str(i)+")\n")
+            file.write(information)
+            file.write("\n")
+        print(f"Content sucessfully appended to {file_name}.")
+    except IOError as e:
+        print(f"An error occured: {e}")
+
+count = 1
+userInput = input("Write the name of the person you would like to learn about today below: \nType exit to leave\n\n")
 while userInput.lower() != 'exit':
     aiResponse = generate_response(userInput)
-    print(aiResponse + "\n")
+    write_to_file(aiResponse, count)
     userInput = input("Who else do you want to learn about? *You can always type exit to leave*\n")
+    count += 1
